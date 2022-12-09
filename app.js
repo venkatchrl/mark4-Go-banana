8700d35f286cbc466017ffd48d62268b47e23ad1
Venkat Marepalli <venkatmarepalli.19@gmail.com>
2022-12-09 20:51:48 +0530

    URL responses

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+    return serverURL + "?" + " text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong try after sometime");
+}
+
+
 function clickEventHandler() {
-    outputDiv.innerText = "dsfsfsf" + txtInput.value;
+    var inputext = txtInput.value;
+
+    fetch(getTranslationURL(inputext))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText
+        })
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
