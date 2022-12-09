57f81cf848290c8776f39fede9d70495c615b0cf
Venkat Marepalli <venkatmarepalli.19@gmail.com>
2022-12-09 18:28:53 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
 function clickEventHandler() {
-    console.log("clicked");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "dsfsfsf" + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
